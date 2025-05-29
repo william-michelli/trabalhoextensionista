b36ce287e6adbc085756e80eecd90ee2868d5b64
Leonardo Barbieri Corso <leobcorso@gmail.com>
2025-05-28 22:29:11 -0300

    Criação da Tela de Login e Layouts
    Criação da Tela de Login e Layouts

---
+++ app/src/main/java/com/aula/trabalhoextensionista/ui/login/LoginViewModelFactory.java
@@ -0,0 +1,26 @@
+package com.aula.trabalhoextensionista.ui.login;
+
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.annotation.NonNull;
+
+import com.aula.trabalhoextensionista.data.LoginDataSource;
+import com.aula.trabalhoextensionista.data.LoginRepository;
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+public class LoginViewModelFactory implements ViewModelProvider.Factory {
+
+    @NonNull
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(LoginViewModel.class)) {
+            return (T) new LoginViewModel(LoginRepository.getInstance(new LoginDataSource()));
+        } else {
+            throw new IllegalArgumentException("Unknown ViewModel class");
+        }
+    }
+}
